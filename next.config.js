3b607e5c384a4d22ae76d8c28544abfbd630e431
John Weland <john.weland@gmail.com>
2022-07-05 13:25:13 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withMarkdoc = require('@markdoc/next.js')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+}
+
+module.exports = withMarkdoc()(nextConfig)
